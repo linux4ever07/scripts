b751c1027652f128747ee6f110be219a42f34b5a
Adam Nilsson <adam.c.nilsson@gmail.com>
2022-07-18 23:12:39 +0200

    minor fixes

--- parse_srt.pl
+++ parse_srt.pl
@@ -30,9 +30,10 @@ sub usage {
 # and prints it without the timestamps.
 sub parse_srt {
 	my $fn = shift;
-	my $skip = 0;
-	my $n = 1;
-	my($this, $next);
+	my $i = 0;
+	my $n = 0;
+	my $switch = 0;
+	my($this, $next, $end, $j);
 	my(@format, @lines);
 
 	$format[0] = qr/^[0-9]+$/;
@@ -40,40 +41,73 @@ sub parse_srt {
 	$format[1] = qr/^$format[1] --> $format[1]$/;
 
 	open(my $srt, '<', $fn) or die "Can\'t open 'SRT': $!";
-	chomp(@lines = (<$srt>));
+	foreach my $line (<$srt>) {
+		$line =~ s/(\r|\n)//g;
+		push(@lines, $line);
+	}
 	close($srt) or die "Can\'t close 'SRT': $!";
 
-	say $fn . "\n\n";
+	say $fn . "\n";
 
-	for (my $i=0; $i < $#lines; $i++) {
-		if ($skip == 1) {
-			$skip = 0;
-			next;
-		}
+	$end = $#lines - 1;
 
-		my $j = $i + 1;
+	until ($i >= $end) {
+		$j = $i + 1;
 
 		$this = $lines[$i];
-		$this =~ s/\r//g;
 		$next = $lines[$j];
-		$next =~ s/\r//g;
 
-		if (length($this)) {
-			if ($this =~ /$format[0]/) {
-				if (length($next)) {
-					if ($next =~ /$format[1]/) {
-						$skip = 1;
-						next;
+		my(@tmp);
+
+		if (length($this) and $this =~ /$format[0]/) {
+			if (length($next) and $next =~ /$format[1]/) {
+				$i = $i + 2;
+				$j = $i + 1;
+
+				$this = $lines[$i];
+				$next = $lines[$j];
+
+				if (length($this)) {
+					push(@tmp, $this);
+				}
+
+				until ($i >= $end) {
+					$i = $i + 1;
+					$j = $i + 1;
+
+					$this = $lines[$i];
+					$next = $lines[$j];
+
+					if (length($this) and $this =~ /$format[0]/) {
+						if (length($next) and $next =~ /$format[1]/) {
+							$switch = 1;
+							last;
+						}
+					}
+
+					if (length($this)) {
+						push(@tmp, $this);
 					}
 				}
+
+				if (scalar(@tmp) > 0) {
+					$n = $n + 1;
+
+					say "";
+
+					foreach my $line (@tmp) {
+						say $n . ': ' . $line;
+					}
+				}
+
+				undef(@tmp);
 			}
 		}
 
-		if (length($this)) {
-			say $n . ': ' . $this;
+		if ($switch eq 0) {
+			$i = $i + 1;
 		} else {
-			++$n;
-			say "";
+			$switch = 0;
 		}
 	}
 }
