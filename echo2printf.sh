e96df70dd6f28342ac4fd3debd2253ab55c8cd86
Adam Nilsson <adam.c.nilsson@gmail.com>
2022-07-19 02:49:35 +0200

    added new script

---
+++ echo2printf.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+# This script is meant to replace echo commands in Bash scripts, with
+# printf. 'echo' is an external command, while 'printf' is a Bash
+# built-in. Hence this can have a performance impact, especially if
+# the script is printing lots of text to the terminal or to files.
+
+# There's still a need to go through the output script file manually
+# after having run it through this script. Because the string that was
+# passed to 'echo' in the input file might have newlines in it. Those
+# newlines need to be added to the '%s' part of the printf command, and
+# removed from the string.
+
+# Also, depending on the use case of the original echo commands, the
+# '\n', in the '%s' part of the printf command might not be necessary.
+# Such as when passing strings between functions.
+
+# The output script file replaces the input file.
+
+if=$(readlink -f "$1")
+bn=$(basename "$if")
+dn=$(dirname "$if")
+
+regex1='^([[:space:]]*)#'
+regex2='echo( \-[[:alpha:]]+){0,} *'
+regex3='printf '\''%s\\n'\'' '
+
+mapfile -t lines <"$if"
+
+for (( i=0; i<${#lines[@]}; i++ )); do
+	line="${lines[${i}]}"
+
+	if [[ $line =~ $regex1 ]]; then
+		continue
+	fi
+
+	if [[ $line =~ $regex2 ]]; then
+		lines[${i}]=$(sed -E "s/${regex2}/${regex3}/g" <<<"$line")
+	fi
+done
+
+truncate -s 0 "$if"
+
+for (( i=0; i<${#lines[@]}; i++ )); do
+	line="${lines[${i}]}"
+
+	printf '%s\n' "$line" >> "$if"
+done
