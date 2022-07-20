9b01b07dabf63f1756a5eea57f9c75f72aa782fc
Adam Nilsson <adam.c.nilsson@gmail.com>
2022-07-20 10:16:01 +0200

    minor fixes

--- tracker_list.sh
+++ tracker_list.sh
@@ -3,13 +3,36 @@
 # duplicates, checks online status of each URL, and prints the list to
 # STDOUT in the correct format.
 
+# The second argument to the script (-nocheck), is optional. If used,
+# the online status of trackers will not be checked, but the list will
+# only get sorted and rid of duplicates.
+
+# Only the trackers that are still online will be printed. This is
+# useful to clean up old lists of public trackers that can be found
+# online, as an example. Though, it might be a good idea to run the
+# script a couple of times, waiting a few hours or days inbetween, since
+# a tracker could be only temporarily offline.
+
+# If you want to save the list in a text file, you can just do a
+# redirection like so:
+
+# tracker_list.sh 'trackers.txt' | tee 'trackers_checked.txt'
+
 usage () {
-	printf '%s\n\n' "Usage: $(basename "$0") [tracker txt]"
+	printf '%s\n\n' "Usage: $(basename "$0") [tracker txt] [-nocheck]"
 	exit
 }
 
 if [[ -z $1 || ! -f $1 ]]; then
 	usage
+elif [[ ! -z $2 && $2 != '-nocheck' ]]; then
+	usage
+fi
+
+nocheck=0
+
+if [[ $2 == '-nocheck' ]]; then
+	nocheck=1
 fi
 
 if=$(readlink -f "$1")
@@ -20,7 +43,7 @@ declare -a trackers
 mapfile -t lines < <(sort --unique <"$if")
 
 for (( i = 0; i < ${#lines[@]}; i++ )); do
-	line="${lines[${i}]}"
+	line=$(tr -d '[:space:]' <<<"${lines[${i}]}")
 	switch=0
 
 	if [[ ! -z $line ]]; then
@@ -49,13 +72,12 @@ done
 declare -A md5h
 
 for (( i = 0; i < ${#trackers[@]}; i++ )); do
-	tracker="${trackers[${i}]}"
-	md5=$(tr -d '[:space:]' <<<"$tracker" | md5sum -)
+	tracker=$(tr -d '[:space:]' <<<"${trackers[${i}]}")
 
-	if [[ ${md5h[${md5}]} -eq 1 ]]; then
+	if [[ $nocheck -eq 1 ]]; then
+		printf '%s\n\n' "$tracker"
+
 		continue
-	else
-		md5h[${md5}]=1
 	fi
 
 	curl --retry 8 --silent --output /dev/null "$tracker"
