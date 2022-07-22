7af0b0e22b716c99fa0cba223e844f453b41c9f5
Adam Nilsson <adam.c.nilsson@gmail.com>
2022-07-22 14:11:59 +0200

    simplified json loop

--- bluray_remux2hevc.sh
+++ bluray_remux2hevc.sh
@@ -410,6 +410,8 @@ imdb () {
 	json_types=(['title']=1 ['year']=1 ['plot']=1)
 
 	for (( z = 0; z < ${#tmp_array[@]}; z++ )); do
+		json_switch=0
+
 		for json_type in "${!json_types[@]}"; do
 			json_regex1_ref="${json_type}_regex1"
 			json_regex2_ref="${json_type}_regex2"
@@ -418,15 +420,20 @@ imdb () {
 				n=$(( z + 1 ))
 				eval ${json_type}=\"$(sed -E "s/${!json_regex2_ref}/\1/" <<<"${tmp_array[${n}]}")\"
 				unset -v json_types[${json_type}]
+				json_switch=1
 				break
 			fi
 		done
+
+		if [[ $json_switch -eq 1 ]]; then
+			break
+		fi
 	done
 
 	printf '%s\n' "$title"
 	printf '%s\n' "$year"
 
-	unset -v title year
+	unset -v title year plot
 }
 
 # Creates a function called 'dts_extract_remux', which will find a
