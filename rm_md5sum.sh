74d7724b2057fd23749897c23929a7b44ace0f67
Adam Nilsson <adam.c.nilsson@gmail.com>
2022-07-24 08:59:02 +0200

    added new scripts

---
+++ rm_md5sum.sh
@@ -0,0 +1,50 @@
+#!/bin/sh
+
+# This script removes duplicate files in the directory given as
+# argument. The files with the oldest modification date will be
+# considered to be the originals, when other files with the same MD5
+# hash are found.
+
+if [[ ! -d $1 ]]; then
+	exit
+fi
+
+dn=$(readlink -f "$1")
+
+mapfile -t files < <(find "$dn" -type f -iname "*" 2>&-)
+
+declare -A md5s_date md5s_fn md5s_og
+
+for (( i = 0; i < ${#files[@]}; i++ )); do
+	fn="${files[${i}]}"
+
+	md5_fn=$(md5sum -b <<<"$fn" | cut -d' ' -f1)
+	md5=$(md5sum -b "$fn" | cut -d' ' -f1)
+	date=$(ls -ldA --time-style=+%s "$fn" | cut -d' ' -f6)
+
+	if [[ ! -z ${md5s_date[${md5}]} ]]; then
+		if [[ $date -lt ${md5s_date[${md5}]} ]]; then
+			md5s_date[${md5}]="$date"
+			md5s_og[${md5}]="$fn"
+		fi
+	else
+		md5s_date[${md5}]="$date"
+		md5s_og[${md5}]="$fn"
+	fi
+
+	md5s_fn[${md5_fn}]="$md5"
+done
+
+for (( i = 0; i < ${#files[@]}; i++ )); do
+	fn="${files[${i}]}"
+
+	md5_fn=$(md5sum -b <<<"$fn" | cut -d' ' -f1)
+	md5="${md5s_fn[${md5_fn}]}"
+
+	if [[ ! -z ${md5s_og[${md5}]} ]]; then
+		if [[ "$fn" != "${md5s_og[${md5}]}" ]]; then
+			printf '%s\n' "$fn"
+			rm -f "$fn"
+		fi
+	fi
+done
---
+++ sub_lang.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+# This script echoes all the subtitle languages of an MKV file
+# in a comma-separated list. The MKV file should be given as the 1st
+# argument to this script.
+
+declare -a mkvinfo_list
+declare -a lang_list
+declare -a lang_list_sorted
+
+if="$1"
+
+if [[ ! -f $if ]]; then
+	printf '%s\n\n' "Usage: $(basename "$0") [MKV]"
+	exit
+fi
+
+command -v mkvinfo >&- || exit
+
+switch=0
+count=0
+
+mapfile -t mkv_info_list < <(mkvinfo "$if")
+
+for (( i = 0; i < ${#mkv_info_list[@]}; i++ )); do
+
+	line="${mkv_info_list[${i}]}"
+
+	if [[ $line =~ 'Track type: subtitles' ]]; then
+		switch=1
+	fi
+
+	if [[ $switch -eq 1 ]]; then
+		if [[ $line =~ 'Language:' ]]; then
+
+			lang_list+=( $(sed 's/^.*Language: //' <<<"$line") )
+			switch=0
+		fi
+
+		if [[ $line =~ 'Name:' ]]; then
+
+			lang_list+=( $(sed 's/^.*Name: //' <<<"$line") )
+			switch=0
+		fi
+	fi
+done
+
+sort_list () {
+	for (( i = 0; i < ${#lang_list[@]}; i++ )); do
+		printf '%s\n' "${lang_list[${i}]}"
+	done | sort
+}
+
+mapfile -t lang_list_sorted < <(sort_list)
+
+for (( i = 0; i < ${#lang_list_sorted[@]}; i++ )); do
+
+	line="${lang_list_sorted[${i}]}"
+
+	if [[ $line ]]; then
+
+		if [[ $count -eq 0 ]]; then
+			printf "Subtitles: %s" "${line^}"
+		else
+			printf ", %s" "${line^}"
+		fi
+
+		let count++
+	fi
+done
+
+printf '\n' 
