a910a019428297ac2d40926c3b7760972fd7c16d
Adam Nilsson <adam.c.nilsson@gmail.com>
2022-07-25 19:12:32 +0200

    minor fixes

--- bluray_remux2hevc.sh
+++ bluray_remux2hevc.sh
@@ -333,10 +333,6 @@ break_name () {
 		name[${i}]=$(tr -d '[:space:]' <<<"${!array_ref}")
 	done
 
-	if [[ ! -z $year ]]; then
-		name+=("$year")
-	fi
-
 # Echoes the complete parsed name.
 	name_string=$(sed -E 's/ +/ /g' <<<"${name[@]}")
 	printf '%s\n' "$name_string"
