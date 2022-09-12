84d86e55070c20583badf87435dfdf371b8e4c20
Adam Nilsson <adam.c.nilsson@gmail.com>
2022-09-12 03:55:06 +0200

    minor fixes

--- md5db_fast.pl
+++ md5db_fast.pl
@@ -78,14 +78,16 @@ my %large :shared;
 # possibly deleted files.
 my %gone_tmp :shared;
 
-# Creating a few shared variables. %err will be used for errors. $n will
-# be used to count the number of files processed. %md5h is the database
-# hash. %file_contents will be used to store the contents of files.
-# $stopping will be used to stop the threads if the script is
-# interrupted. $file_stack will be used to track the amount of file data
-# currently in RAM. $busy will be used to pause other threads when a
-# thread is busy. $disk_size will be used to limit the amount of file
-# data that can be read into RAM.
+# Creating a few shared variables.
+# * %err will be used for errors.
+# * $n will be used to count the number of files processed.
+# * %md5h is the database hash.
+# * %file_contents will be used to store the contents of files.
+# * $stopping will be used to stop the threads if the script is
+# interrupted.
+# * $file_stack will be used to track the amount of file data currently
+# in RAM.
+# * $busy will be used to pause other threads when a thread is busy.
 my %err :shared;
 my $n :shared = 0;
 my %md5h :shared;
