dfbe2fabed46704dc40a01562d7bd0a5ad20f9ff
Adam Nilsson <adam.c.nilsson@gmail.com>
2022-10-31 18:27:28 +0100

    minor fixes

--- bluray_remux2hevc.sh
+++ bluray_remux2hevc.sh
@@ -464,8 +464,8 @@ dts_extract_remux () {
 	regex_51=', 5.1\(.*\),'
 
 	regex_stream='^ +Stream #'
-	regex_bps='^ +BPS.*: ([0-9]+)$'
 	regex_kbps=', ([0-9]+) kb\/s'
+	regex_bps='^ +BPS.*: ([0-9]+)$'
 	regex_last3='^[0-9]+([0-9]{3})$'
 	regex_zero='^0+([0-9]+)$'
 
