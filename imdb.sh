d872c6801549578da375e1f8f3c7a61819750f61
Adam Nilsson <adam.c.nilsson@gmail.com>
2022-11-14 07:14:30 +0100

    minor fixes

--- imdb.sh
+++ imdb.sh
@@ -64,7 +64,8 @@ imdb () {
 		return 1
 	fi
 
-	term=("$@")
+	mapfile -t term < <(sed -E 's/[[:blank:]]\+/ /g' "$@")
+	term[-1]="${term[-1]%$'\n'}"
 	y_regex='^\(([0-9]{4})\)$'
 
 	id_regex='\/title\/(tt[0-9]+)'
