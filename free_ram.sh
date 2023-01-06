e80bb9f5da3023ce66fde9f1b77cb411995c1ead
Adam Nilsson <adam.c.nilsson@gmail.com>
2023-01-06 13:11:26 +0100

    cleaner code, firefox / tor works again

--- free_ram.sh
+++ free_ram.sh
@@ -20,8 +20,10 @@ now () { date '+%F %H:%M:%S'; }
 
 ram_limit=1000000
 
+regex_pid_args='^[[:blank:]]*([0-9]+)[[:blank:]]*([^ ]+).*$'
 regex_rend='--type=renderer'
 regex_ext='--extension-process'
+regex_tab='^.*\-childID [0-9]+.* tab$'
 
 # Creates a file name for the log.
 log_killed="${HOME}/browser_killed.log"
@@ -31,87 +33,102 @@ if [[ ! -f $log_killed ]]; then
 	touch "$log_killed"
 fi
 
-# Creates a function called 'is_chromium', which checks if Chromium
-# or Chrome is running.
-is_chromium () {
-	for comm in chrome chromium; do
-		pgrep -x "$comm" | tr -d '[:blank:]'
+# Creates a function called 'get_pids', which gets all child process IDs
+# of the command names given to it as arguments.
+get_pids () {
+	declare -A pids
+
+	for comm in "$@"; do
+		regex_comm="^.*${comm}$"
+
+		mapfile -t session < <(ps -C "$comm" -o sid= | tr -d '[:blank:]' | sort -u)
+
+		if [[ ${#session[@]} -eq 0 ]]; then
+			continue
+		fi
+
+		mapfile -t child < <(ps -g "${session[0]}" -o pid=,args=)
+
+		for (( i = 0; i < ${#child[@]}; i++ )); do
+			line="${child[${i}]}"
+
+			if [[ $line =~ $regex_pid_args ]]; then
+				pid="${BASH_REMATCH[1]}"
+				args="${BASH_REMATCH[2]}"
+
+				if [[ $pid -eq ${session[0]} ]]; then
+					continue
+				fi
+
+				if [[ $args =~ $regex_comm ]]; then
+					pids["${pid}"]="$args"
+				fi
+			fi
+		done
 	done
+
+	printf '%s\n' "${!pids[@]}" | sort -n
 }
 
 # Creates a function called 'kill_chromium', which kills all child
 # processes belonging to either Chromium or Chrome.
 kill_chromium () {
-	pid_switch=0
+	declare -a pids_tmp
 
-	for comm in chrome chromium; do
-		mapfile -t parent < <(ps -C "$comm" -o ppid | tail -n +2 | tr -d '[:blank:]' | sort -u)
-		mapfile -t child < <(ps -C "$comm" -o pid | tail -n +2 | tr -d '[:blank:]')
+	mapfile -t pids_tmp < <(get_pids 'chrome' 'chromium')
 
-		for (( i = 0; i < ${#child[@]}; i++ )); do
-			if [[ -n ${child[${i}]} ]]; then
-				child_pid="${child[${i}]}"
+	if [[ ${#pids_tmp[@]} -eq 0 ]]; then
+		return
+	fi
 
-				for (( j = 0; j < ${#parent[@]}; j++ )); do
-					if [[ -n ${parent[${j}]} ]]; then
-						parent_pid="${parent[${j}]}"
+	time=$(now)
+	printf '%s\n\n' "${time}: Killing Chrome / Chromium..." | tee --append "$log_killed"
 
-						if [[ $parent_pid == "$child_pid" ]]; then
-							pid_switch=1
-						fi
-					fi
-				done
+	for (( i = 0; i < ${#pids_tmp[@]}; i++ )); do
+		pid="${pids_tmp[${i}]}"
+		args=$(ps -p "$pid" -o args=)
 
 # Adding an extra check, which checks if $child_pid is a renderer /
-# extension process. If it's NOT a renderer, set $pid_switch to '1',
-# preventing it from being killed. If the process is an extension
-# process, also set $pid_switch to '1'. This will keep extensions and
-# downloads running, even though the other Chrome child processes are
-# killed. Only renderer processes that are NOT extension processes will
-# get killed.
-				chrome_type=$(ps -p "$child_pid" -o args | tail -n +2)
+# extension process. If it's NOT a renderer, or is an extension
+# process, skip it. This will keep extensions and downloads running,
+# even though the other Chrome child processes are killed. Only renderer
+# processes that are NOT extension processes will get killed.
+		if [[ ! $args =~ $regex_rend ]]; then
+			continue
+		elif [[ $args =~ $regex_ext ]]; then
+			continue
+		fi
 
-				if [[ ! $chrome_type =~ $regex_rend ]]; then
-					pid_switch=1
-				elif [[ $chrome_type =~ $regex_ext ]]; then
-					pid_switch=1
-				fi
-
-				if [[ $pid_switch -eq 0 ]]; then
-					printf '%s\n' "SIGKILL: ${child_pid}"
-					kill -9 "$child_pid"
-				else
-					pid_switch=0
-				fi
-			fi
-		done
+		printf '%s\n' "SIGKILL: ${pid}"
+		kill -9 "$pid"
 	done
 }
 
 # Creates a function called 'kill_firefox', which kills all child
 # processes belonging to either Firefox or Tor Browser.
 kill_firefox () {
-	declare -a pids_tmp pids
+	declare -a pids_tmp
 
-	for name in 'Web Content' 'WebExtensions'; do
-		mapfile -t pids_tmp < <(pgrep -x "$name" | tr -d '[:blank:]')
-		pids+=("${pids_tmp[@]}")
+	mapfile -t pids_tmp < <(get_pids 'firefox' 'firefox.real')
+
+	if [[ ${#pids_tmp[@]} -eq 0 ]]; then
+		return
+	fi
+
+	time=$(now)
+	printf '%s\n\n' "${time}: Killing Firefox / Tor Browser..." | tee --append "$log_killed"
+
+	for (( i = 0; i < ${#pids_tmp[@]}; i++ )); do
+		pid="${pids_tmp[${i}]}"
+		args=$(ps -p "$pid" -o args=)
+
+		if [[ ! $args =~ $regex_tab ]]; then
+			continue
+		fi
+
+		printf '%s\n' "SIGKILL: ${pid}"
+		kill -9 "$pid"
 	done
-
-	for name in firefox tor; do
-		for (( i = 0; i <= ${#pids[@]}; i++ )); do
-			if [[ -n ${pids[${i}]} ]]; then
-				pid="${pids[${i}]}"
-				p_name=$(ps -p "$pid" -o comm | tail -n +2)
-
-				if [[ $p_name == "$name" ]]; then
-					unset -v pids["${i}"]
-				fi
-			fi
-		done
-	done
-
-	kill -9 "${pids[@]}"
 }
 
 # Creates an infinite while loop.
@@ -136,39 +153,13 @@ while true; do
 
 # If available RAM is less than 1GB...
 	if [[ ${ram[6]} -lt $ram_limit ]]; then
-# Checks if Firefox and Chromium are running. $is_chromium is an array,
-# since the output probably spans across multiple lines, due to Chromium
-# being highly multithreaded and keeping separate threads / processes
-# for every window and tab.
-		is_firefox=$(pgrep -x firefox | tr -d '[:blank:]')
-		is_tor=$(pgrep -x tor | tr -d '[:blank:]')
-		mapfile -t is_chromium < <(is_chromium)
+# If Firefox / Tor Browser is running, then kill it, print a message to
+# the screen, and append a message to the log.
+		kill_firefox
 
-# If Firefox is running, then kill it, print a message to the screen,
-# and append a message to the log.
-		if [[ -n $is_firefox ]]; then
-			time=$(now)
-
-			printf '%s\n\n' "${time}: Killing Firefox..." | tee --append "$log_killed"
-			kill_firefox
-		fi
-
-# If Tor Browser is running, then kill it, print a message to the
+# If Chrome / Chromium is running, then kill it, print a message to the
 # screen, and append a message to the log.
-		if [[ -n $is_tor ]]; then
-			time=$(now)
-
-			printf '%s\n\n' "${time}: Killing Tor Browser..." | tee --append "$log_killed"
-			kill_firefox
-		fi
-
-# If Chromium is running, then...
-		if [[ -n ${is_chromium[0]} ]]; then
-			time=$(now)
-
-			printf '%s\n\n' "${time}: Killing Chromium..." | tee --append "$log_killed"
-			kill_chromium
-		fi
+		kill_chromium
 
 # Writes cached writes to disk. Hopefully this will also clear up a
 # little RAM.
