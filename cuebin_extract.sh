cb747853b275e5d3a90e32ce8b1e219b9c73f301
Adam Nilsson <adam.c.nilsson@gmail.com>
2023-03-05 18:41:43 +0100

    minor fixes

--- cuebin_extract.sh
+++ cuebin_extract.sh
@@ -895,7 +895,7 @@ clean_up () {
 }
 
 # Checks if 'oggenc', 'flac' are installed. Depending on which mode is
-# set, check if 'bchunk' or 'sox' is installed.
+# set, check if 'bchunk', 'ffmpeg' or 'sox' is installed.
 check_cmd 'oggenc' 'flac' "$mode"
 
 # Creates the output directory and changes into it.
