ff804553edac8c6ddeba1b0bb182610ea86ca426
Adam Nilsson <adam.c.nilsson@gmail.com>
2023-06-19 15:22:18 +0200

    added new script

---
+++ rm_quotes.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+# This script will recursively change the file / directory names under
+# the directory specified, to remove quotes and double quotes from file
+# / directory names.
+
+set -eo pipefail
+
+usage () {
+	printf '\n%s\n\n' "Usage: $(basename "$0") [dir]"
+	exit
+}
+
+if [[ ! -d $1 ]]; then
+	usage
+fi
+
+dir=$(readlink -f "$1")
+depth_max=0
+
+mapfile -d'/' -t path_parts <<<"$dir"
+depth_og=$(( ${#path_parts[@]} - 1 ))
+
+mapfile -t files < <(find "$dir" 2>&-)
+
+for (( i = 0; i < ${#files[@]}; i++ )); do
+	fn="${files[${i}]}"
+
+	mapfile -d'/' -t path_parts <<<"$fn"
+	depth_tmp=$(( ${#path_parts[@]} - 1 ))
+	depth_diff=$(( depth_tmp - depth_og ))
+
+	if [[ $depth_diff -gt $depth_max ]]; then
+		depth_max="$depth_diff"
+	fi
+done
+
+unset -v files path_parts depth_og depth_tmp depth_diff
+
+for (( i = depth_max; i > 0; i-- )); do
+	mapfile -t files < <(find "$dir" -mindepth "$i" -maxdepth "$i" 2>&-)
+
+	for (( j = 0; j < ${#files[@]}; j++ )); do
+		fn="${files[${j}]}"
+		dn=$(dirname "$fn")
+		bn=$(basename "$fn")
+
+		new_bn=$(printf '%s' "$bn" | tr -d "'" | tr -d '"')
+		new_fn="${dn}/${new_bn}"
+
+		if [[ $new_bn != "$bn" ]]; then
+			printf '%s\n' "$new_fn"
+			mv -n "$fn" "$new_fn"
+		fi
+	done
+done
