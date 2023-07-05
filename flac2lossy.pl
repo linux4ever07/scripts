21a32ec8876858dd21e34527a984382d7d71c36e
Adam Nilsson <adam.c.nilsson@gmail.com>
2023-07-06 01:56:27 +0200

    added new script

---
+++ flac2lossy.pl
@@ -0,0 +1,565 @@
+#!/usr/bin/perl
+
+# This script is a multithreaded batch encoder. It encodes FLAC files to
+# lossy codecs / formats.
+
+# Currently supported codecs are:
+
+# * MP3 (lame)
+# * AAC (ffmpeg)
+# * Ogg Vorbis (oggenc)
+# * Opus (opusenc)
+
+# The script assumes the input is Stereo, 44.1 kHz.
+
+# The FLAC files are decoded, and stored in RAM up to the limit defined
+# in $disk_size. The $file_stack variable is used to count the total
+# amount of data decoded and stored in RAM at any time.
+
+# The script looks for the CPU core count in /proc/cpuinfo and starts
+# as many threads as it finds cores. The threads are left
+# waiting for files to be enqueued.
+
+use 5.34.0;
+use strict;
+use warnings;
+use Cwd qw(abs_path);
+use File::Path qw(make_path);
+use File::Basename qw(basename);
+
+use threads qw(yield);
+use threads::shared;
+use Thread::Queue;
+
+my %pcm :shared;
+
+my $file_stack :shared = 0;
+my $disk_size = 1000000000;
+
+my $files_q = Thread::Queue->new();
+
+my(%regex, %files, @dirs, @opts, @threads, $mode);
+
+$regex{newline} = qr/(\r){0,}(\n){0,}$/;
+$regex{date} = qr/^([0-9]{4})/;
+$regex{quote} = qr/^(\")|(\")$/;
+$regex{space} = qr/(^\s*)|(\s*$)/;
+$regex{tag} = qr/^([^=]+)=(.*)$/;
+
+my @required_tags = qw(albumartist album date discnumber tracknumber title);
+
+# Get the number of available CPU cores. Add 1 to this number, which
+# will lead to an extra thread being created, maximizing performance.
+chomp(my $cores = `grep -c '^processor' '/proc/cpuinfo'`);
+$cores++;
+
+# The 'usage' subroutine prints usage instructions, and then quits.
+sub usage {
+	say '
+Usage: ' . basename($0) . ' [format] [FLAC directory 1] .. [FLAC directory N]
+
+	Formats:
+-mp3
+	Encode to the MP3 audio format (using lame)
+-aac
+	Encode to the AAC audio format (using ffmpeg)
+-ogg
+	Encode to the Ogg Vorbis audio format (using oggenc)
+-opus
+	Encode to the Opus audio format (using opusenc)
+';
+
+	exit;
+}
+
+# Choose script mode (codec) based on arguments given to the script.
+given (my $arg = shift(@ARGV)) {
+	when ('-mp3') {
+		$mode = 'mp3';
+
+		@opts = ('lame', '-q 0', '-V 2', '--silent', '--id3v2-only');
+	}
+	when ('-aac') {
+		$mode = 'aac';
+
+		@opts = ('ffmpeg', '-loglevel', 'fatal', '-f', 's16le', '-ar', '44.1k', '-ac', '2', '-i', 'pipe:', '-strict', '-2', '-c:a', 'aac', '-b:a', '192k', '-profile:a', 'aac_ltp');
+	}
+	when ('-ogg') {
+		$mode = 'ogg';
+
+		@opts = ('oggenc', '--quiet', '-q 6');
+	}
+	when ('-opus') {
+		$mode = 'opus';
+
+		@opts = ('opusenc', '--quiet', '--bitrate', '160', '--vbr', '--music', '--comp', '10');
+	}
+	default { usage(); }
+}
+
+# If the remaining arguments are directories, store them in the @dirs
+# array.
+while (my $arg = shift(@ARGV)) {
+	if (-d $arg) {
+		my $dn = abs_path($arg);
+		getdirs($dn);
+	} else { usage(); }
+}
+
+# Print usage instructions if @dirs array is empty.
+if (! scalar(@dirs)) { usage(); }
+
+foreach my $dn (@dirs) { getfiles($dn); }
+
+check_cmd('flac', 'metaflac', 'lame', 'ffmpeg', 'oggenc', 'opusenc');
+
+# The 'check_cmd' subroutine checks if the necessary commands are
+# installed. If any of the commands are missing, print them and quit.
+sub check_cmd {
+	my(@missing_pkg, $stdout);
+
+	while (my $cmd = shift(@_)) {
+		`command -v "$cmd" 1>&-`;
+
+		if ($? != 0) {
+			push(@missing_pkg, $cmd);
+		}
+	}
+
+	if (scalar(@missing_pkg) > 0) {
+		say "\n" . 'You need to install the following through your package manager:' . "\n";
+		print(join("\n", @missing_pkg));
+		say "\n";
+		exit;
+	}
+}
+
+# The 'gettags' subroutine reads the tags from a FLAC file.
+sub gettags {
+	my $fn = shift;
+
+	my(%alltags, @lines);
+
+	open(my $output, '-|', 'metaflac', '--no-utf8-convert', '--export-tags-to=-', $fn)
+	or die "Can't open metaflac: $!";
+	chomp(@lines = (<$output>));
+	close($output) or die "Can't close metaflac: $!";
+
+	while (my $line = shift(@lines)) {
+		my($field, $value);
+
+		$line =~ s/$regex{quote}//g;
+
+		if ($line =~ m/$regex{tag}/) {
+			$field = lc($1);
+			$value = $2;
+
+			$field =~ s/$regex{space}//g;
+			$value =~ s/$regex{space}//g;
+			$value =~ tr/a-zA-Z0-9\.\-_ //dc;
+			$value =~ s/\s+/ /g;
+
+			if ($field eq 'album' or $field eq 'albumartist') {
+				$value =~ s/^\.+//g;
+			}
+		}
+
+		if (! length($field) or ! length($value)) { next; }
+
+		push(@{$alltags{$field}}, $value);
+	}
+
+	return(%alltags);
+}
+
+# The 'getdirs' subroutine finds all the sub-directories under the FLAC
+# directories given as arguments to the script.
+sub getdirs {
+	my $dn = shift;
+
+	my(@lines);
+
+	open(my $find, '-|', 'find', $dn, '-type', 'd', '-nowarn')
+	or die "Can't run 'find': $!";
+	chomp(@lines = (<$find>));
+	close($find) or die "Can't close 'find': $!";
+
+	push(@dirs, @lines);
+}
+
+# The 'getfiles' subroutine gets a list of FLAC files in the directory
+# passed to it.
+sub getfiles {
+	my $dn = shift;
+
+	my(%tags);
+
+	opendir(my $dh, $dn) or die "Can't open directory '$dn': $!";
+	foreach my $bn (readdir($dh)) {
+		my $fn = $dn . '/'. $bn;
+
+		if (! -f $fn) { next; }
+
+		if ($bn =~ /\.flac$/i) { $tags{$fn} = { gettags($fn) }; }
+	}
+	closedir $dh or die "Can't close directory '$dn': $!";
+
+	foreach my $fn (keys(%tags)) {
+		my $tags_ref = \$tags{$fn};
+
+		existstag($fn, $tags_ref, @required_tags);
+
+		$pcm{$fn} = 1;
+
+		foreach my $field (keys(%{$$tags_ref})) {
+			$files{$fn}{$field} = $$tags_ref->{$field}[0];
+		}
+	}
+}
+
+# The 'existstag' subroutine checks for the existence of required tags.
+# If it doesn't find them, it quits.
+sub existstag {
+	my $fn = shift;
+	my $tags_ref = shift;
+
+	my(@missing_tags);
+
+	while (my $field = shift(@_)) {
+		if (! length($$tags_ref->{$field}[0])) {
+			push(@missing_tags, $field);
+		}
+	}
+
+	if (! scalar(@missing_tags)) {
+		return;
+	}
+
+	foreach my $field (@missing_tags) {
+		say $fn . ': missing ' . $field . ' tag';
+	}
+
+	exit;
+}
+
+# The 'mk_refs' subroutine creates references for other subroutines to
+# have easier access to tags.
+sub mk_refs {
+	my $fn = shift;
+	my $tags_ref = shift;
+
+	$$tags_ref{date} = \$files{$fn}{discnumber};
+	$$tags_ref{discnumber} = \$files{$fn}{discnumber};
+	$$tags_ref{tracknumber} = \$files{$fn}{tracknumber};
+	$$tags_ref{artist} = \$files{$fn}{artist};
+	$$tags_ref{albumartist} = \$files{$fn}{albumartist};
+	$$tags_ref{album} = \$files{$fn}{album};
+	$$tags_ref{title} = \$files{$fn}{title};
+}
+
+# The 'files2queue' subroutine puts files in the queue, and loads them
+# into RAM.
+sub files2queue {
+	my($size, $free);
+
+	foreach my $fn (keys(%files)) {
+		$size = (stat($fn))[7];
+
+		$free = $disk_size - $file_stack;
+
+# If file size is bigger than the amount of free RAM, wait.
+		while ($size > $free) {
+			yield();
+			$free = $disk_size - $file_stack;
+		}
+
+		$size = decode($fn);
+
+		$files_q->enqueue($fn, $size);
+	}
+
+	$files_q->end();
+}
+
+# The 'name' subroutine generates file names for output files.
+sub name {
+	my $fn = shift;
+	my $ext = shift;
+	my $tags_ref = shift;
+
+	my(@tags, $of, $of_bn, $of_dn);
+
+	sub rm_special_chars {
+		my $string = shift;
+		$string =~ tr/a-zA-Z0-9\.\-_ //dc;
+
+		return($string);
+	}
+
+	push(@tags, rm_special_chars(${$$tags_ref{albumartist}}));
+	push(@tags, rm_special_chars(${$$tags_ref{album}}));
+	push(@tags, rm_special_chars(${$$tags_ref{discnumber}}));
+	push(@tags, rm_special_chars(${$$tags_ref{tracknumber}}));
+	push(@tags, rm_special_chars(${$$tags_ref{title}}));
+
+	$of_dn = join('/', $ENV{HOME}, $ext, $tags[0], $tags[1]);
+
+	unless (-d $of_dn) {
+		make_path($of_dn) or warn "Can't make_path '$of_dn': $!";
+	}
+
+	$of_bn = sprintf('%d-%02d. %s.%s', $tags[2], $tags[3], $tags[4], $ext);
+
+	$of = $of_dn . '/' . $of_bn;
+
+	if (-f $of) {
+		say $of . ': already exists';
+		threads->exit();
+	}
+
+	return($of);
+}
+
+# The 'decode' subroutine decodes FLAC files to WAV / PCM, and stores
+# them in RAM.
+sub decode {
+	my $fn = shift;
+
+	my($size);
+
+# Enable slurp mode.
+	local $/;
+
+	open(my $flac, '-|:raw', 'flac', '--silent', '--stdout',
+	'--decode', $fn) or die "can't run 'flac': $!";
+	$pcm{$fn} = (<$flac>);
+	close($flac) || die "couldn't close 'flac': $!";
+
+	$size = length($pcm{$fn});
+
+	{ lock($file_stack);
+	$file_stack += $size; }
+
+	return($size);
+}
+
+# The 'lame' subroutine encodes FLAC files to MP3.
+sub lame {
+	my $tid = threads->tid();
+	my @old_opts = @opts;
+
+	while (my($if, $size) = $files_q->dequeue(2)) {
+		my(%tags_ref, $tag_tmp);
+		mk_refs($if, \%tags_ref);
+
+		my $of = name($if, 'mp3', \%tags_ref);
+
+		$tag_tmp = ${$tags_ref{artist}};
+		push(@opts, ('--ta', $tag_tmp));
+
+		$tag_tmp = ${$tags_ref{album}};
+		push(@opts, ('--tl', $tag_tmp));
+
+		$tag_tmp = ${$tags_ref{tracknumber}};
+		push(@opts, ('--tn', $tag_tmp));
+
+		$tag_tmp = ${$tags_ref{title}};
+		push(@opts, ('--tt', $tag_tmp));
+
+		${$tags_ref{date}} =~ /$regex{date}/;
+
+		if (length($1)) {
+			$tag_tmp = $1;
+			push(@opts, ('--ty', $tag_tmp));
+		}
+
+		push(@opts, '-');
+		push(@opts, $of);
+
+		say $tid . ' ' . $of . ': encoding...';
+
+		open(my $lame, '|-:raw', @opts)
+		or die "can't run 'lame': $!";
+		print $lame $pcm{$if};
+		close($lame) || die "couldn't close 'lame': $!";
+
+		@opts = @old_opts;
+
+		{ lock(%pcm);
+		lock($file_stack);
+		delete($pcm{$if});
+		$file_stack -= $size; }
+	}
+}
+
+# The 'aac' subroutine encodes FLAC files to AAC.
+sub aac {
+	my $tid = threads->tid();
+	my @old_opts = @opts;
+
+	while (my($if, $size) = $files_q->dequeue(2)) {
+		my(%tags_ref, $tag_tmp);
+		mk_refs($if, \%tags_ref);
+
+		my $of = name($if, 'm4a', \%tags_ref);
+
+		$tag_tmp = 'artist' . '=' . ${$tags_ref{artist}};
+		push(@opts, ('-metadata', $tag_tmp));
+
+		$tag_tmp = 'album' . '=' . ${$tags_ref{album}};
+		push(@opts, ('-metadata', $tag_tmp));
+
+		$tag_tmp = 'tracknumber' . '=' . ${$tags_ref{tracknumber}};
+		push(@opts, ('-metadata', $tag_tmp));
+
+		$tag_tmp = 'title' . '=' . ${$tags_ref{title}};
+		push(@opts, ('-metadata', $tag_tmp));
+
+		${$tags_ref{date}} =~ /$regex{date}/;
+
+		if (length($1)) {
+			$tag_tmp = 'date' . '=' . $1;
+			push(@opts, ('-metadata', $tag_tmp));
+		}
+
+		push(@opts, $of);
+
+		say $tid . ' ' . $of . ': encoding...';
+
+		open(my $ffmpeg, '|-:raw', @opts)
+		or die "can't run 'ffmpeg': $!";
+		print $ffmpeg $pcm{$if};
+		close($ffmpeg) || die "couldn't close 'ffmpeg': $!";
+
+		@opts = @old_opts;
+
+		{ lock(%pcm);
+		lock($file_stack);
+		delete($pcm{$if});
+		$file_stack -= $size; }
+	}
+}
+
+# The 'vorbis' subroutine encodes FLAC files to Ogg Vorbis.
+sub vorbis {
+	my $tid = threads->tid();
+	my @old_opts = @opts;
+
+	while (my($if, $size) = $files_q->dequeue(2)) {
+		my(%tags_ref, $tag_tmp);
+		mk_refs($if, \%tags_ref);
+
+		my $of = name($if, 'ogg', \%tags_ref);
+
+		push(@opts, ('-o', $of));
+
+		$tag_tmp = 'artist' . '=' . ${$tags_ref{artist}};
+		push(@opts, ('-c', $tag_tmp));
+
+		$tag_tmp = 'album' . '=' . ${$tags_ref{album}};
+		push(@opts, ('-c', $tag_tmp));
+
+		$tag_tmp = 'tracknumber' . '=' . ${$tags_ref{tracknumber}};
+		push(@opts, ('-c', $tag_tmp));
+
+		$tag_tmp = 'title' . '=' . ${$tags_ref{title}};
+		push(@opts, ('-c', $tag_tmp));
+
+		${$tags_ref{date}} =~ /$regex{date}/;
+
+		if (length($1)) {
+			$tag_tmp = 'date' . '=' . $1;
+			push(@opts, ('-c', $tag_tmp));
+		}
+
+		push(@opts, '-');
+
+		say $tid . ' ' . $of . ': encoding...';
+
+		open(my $oggenc, '|-:raw', @opts)
+		or die "can't run 'oggenc': $!";
+		print $oggenc $pcm{$if};
+		close($oggenc) || die "couldn't close 'oggenc': $!";
+
+		@opts = @old_opts;
+
+		{ lock(%pcm);
+		lock($file_stack);
+		delete($pcm{$if});
+		$file_stack -= $size; }
+	}
+}
+
+# The 'opus' subroutine encodes FLAC files to Opus.
+sub opus {
+	my $tid = threads->tid();
+	my @old_opts = @opts;
+
+	while (my($if, $size) = $files_q->dequeue(2)) {
+		my(%tags_ref, $tag_tmp);
+		mk_refs($if, \%tags_ref);
+
+		my $of = name($if, 'opus', \%tags_ref);
+
+		$tag_tmp = 'artist' . '=' . ${$tags_ref{artist}};
+		push(@opts, ('--comment', $tag_tmp));
+
+		$tag_tmp = 'album' . '=' . ${$tags_ref{album}};
+		push(@opts, ('--comment', $tag_tmp));
+
+		$tag_tmp = 'tracknumber' . '=' . ${$tags_ref{tracknumber}};
+		push(@opts, ('--comment', $tag_tmp));
+
+		$tag_tmp = 'title' . '=' . ${$tags_ref{title}};
+		push(@opts, ('--comment', $tag_tmp));
+
+		${$tags_ref{date}} =~ /$regex{date}/;
+
+		if (length($1)) {
+			$tag_tmp = 'date' . '=' . $1;
+			push(@opts, ('--comment', $tag_tmp));
+		}
+
+		push(@opts, '-');
+		push(@opts, $of);
+
+		say $tid . ' ' . $of . ': encoding...';
+
+		open(my $opusenc, '|-:raw', @opts)
+		or die "can't run 'opusenc': $!";
+		print $opusenc $pcm{$if};
+		close($opusenc) || die "couldn't close 'opusenc': $!";
+
+		@opts = @old_opts;
+
+		{ lock(%pcm);
+		lock($file_stack);
+		delete($pcm{$if});
+		$file_stack -= $size; }
+	}
+}
+
+say "\n" . 'Starting threads!' . "\n";
+
+push(@threads, threads->create(\&files2queue));
+
+foreach (1 .. $cores) {
+	given ($mode) {
+		when ('mp3') {
+			push(@threads, threads->create(\&lame));
+		}
+		when ('aac') {
+			push(@threads, threads->create(\&aac));
+		}
+		when ('ogg') {
+			push(@threads, threads->create(\&vorbis));
+		}
+		when ('opus') {
+			push(@threads, threads->create(\&opus));
+		}
+	}
+}
+
+foreach my $thr (@threads) { $thr->join(); }
+
+say "\n" . 'Done!' . "\n";
