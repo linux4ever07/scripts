fde9ac26af6a36245ab80e78449c3422dd20ed5e
Adam Nilsson <adam.c.nilsson@gmail.com>
2023-07-24 11:26:07 +0200

    minor fixes

--- dnf_reinstall.sh
+++ dnf_reinstall.sh
@@ -4,14 +4,7 @@
 # verifies all RPMs to see which ones are broken, and reinstalls those.
 # Run with either 'full' or 'verify' as an argument.
 
-declare mode date txt
-declare -A regex
-
-date=$(date '+%F')
-txt="${HOME}/dnf_reinstall_${date}.txt"
-
-regex[dnf]='^([^ ]+).*$'
-regex[rpm]='^[^/]+(.*)$'
+declare mode
 
 # Creates a function called 'usage', which will print usage instructions
 # and then quit.
@@ -38,10 +31,19 @@ if [[ $EUID -ne 0 ]]; then
 	exit
 fi
 
+declare date txt_fn
+declare -A regex
+
+date=$(date '+%F')
+txt_fn="${HOME}/dnf_reinstall_${date}.txt"
+
+regex[dnf]='^([^ ]+).*$'
+regex[rpm]='^[^/]+(.*)$'
+
 # Creates a function called 'dnf_install', which will reinstall
 # packages.
 dnf_install () {
-	touch "$txt"
+	touch "$txt_fn"
 
 	for (( i = 0; i < ${#dnf_pkgs[@]}; i++ )); do
 		rpm="${dnf_pkgs[${i}]}"
@@ -49,7 +51,7 @@ dnf_install () {
 		dnf -y reinstall "$rpm"
 
 		if [[ $? -eq 0 ]]; then
-			printf '%s\n' "$rpm" >> "$txt"
+			printf '%s\n' "$rpm" >> "$txt_fn"
 		fi
 	done
 }
