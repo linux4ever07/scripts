77d4f93a0d409a45fdf46e345754b77562d0fce5
Adam Nilsson <adam.c.nilsson@gmail.com>
2023-09-01 04:45:50 +0200

    minor fixes

--- return_of_the_triad.sh
+++ return_of_the_triad.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# This script is a launcher for Rise of the Triad: Dark War (DOOM mod)
-# for GZDoom.
+# This script is a launcher for Return of the Triad (DOOM mod), and
+# fan-madesequel to Rise of the Triad: Dark War, for GZDoom.
 
 # https://www.moddb.com/mods/return-of-the-triad
 
@@ -11,14 +11,14 @@ declare doom_wad doom2_wad iwad wad
 # Change the path variables below, to point to the correct directories
 # for:
 # * GZDoom configuration directory
-# * Rise of the Triad: Dark War PK3 file directory
+# * Return of the Triad PK3 file directory
 gzdoom_cfg="${HOME}/.var/app/org.zdoom.GZDoom/.config/gzdoom"
 rott_dn="${gzdoom_cfg}/rott"
 
 # * The Ultimate DOOM WAD
 # * DOOM 2 WAD
 # * Fake IWAD
-# * Rise of the Triad: Dark War WAD
+# * Return of the Triad WAD
 doom_wad="${gzdoom_cfg}/doom.wad"
 doom2_wad="${gzdoom_cfg}/doom2.wad"
 iwad="${rott_dn}/fakeiwad.wad"
